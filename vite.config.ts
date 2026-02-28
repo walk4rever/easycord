0aa943d005bdaefe0db78f4be6558d85823771ba
Rafael129 <walkklaw@gmail.com>
2026-02-28 12:08:33 +0800

    fix: set base path to /easycord/ for subpath deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/easycord/',
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
   },
